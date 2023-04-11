a07309b9856db7dad9838426da30a31ef9137f24
hare1901 <hare1901@yandex.ru>
2023-04-11 15:21:56 +0300

    modern static in project

--- products/views.py
+++ products/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
-    return render(request,"products/index.html")
+    context = {"title": "Magazine Store"}
+
+    return render(request, "products/index.html", context)
+
 
 def products(request):
-    return rende(request, "product/products.html")
+    context = {"title": "Store - Каталог"}
+    return render(request, "product/products.html", context)
