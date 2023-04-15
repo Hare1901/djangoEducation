1129a100f628d44c73520dc9f98881f48ec4087b
hare1901 <hare1901@yandex.ru>
2023-04-15 13:46:28 +0300

    use dinamic url

--- products/views.py
+++ products/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context = {"title": "Magazine Store"}
+    context = {"title": "Vfufpby"}
 
     return render(request, "products/index.html", context)
 
 
 def products(request):
     context = {"title": "Store - Каталог"}
-    return render(request, "product/products.html", context)
+    return render(request, "products/products.html", context)
