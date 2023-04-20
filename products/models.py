5a64f58a4a7f4495e5a0fc6fe98819cdab51ca62
hare1901 <hare1901@yandex.ru>
2023-04-20 19:45:44 +0300

    again

--- products/models.py
+++ products/models.py
@@ -5,10 +5,17 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
-    categoru = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Продукт: {self.name}| категория: {self.category.name}"
