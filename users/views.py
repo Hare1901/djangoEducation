67ee5b46ade69b43835321b7e6f4c04b9b614424
hare1901 <hare1901@yandex.ru>
2023-05-05 06:40:27 +0300

    обработка ошибок регистрации и вывод сообщения

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 from users.forms import UserLoginForm, UserRegistrationForm, UseProfileForm
 
 
@@ -25,6 +25,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Аккаунт зарегистрирован!")
             return HttpResponseRedirect(reverse("users:login"))
     else:
         form = UserRegistrationForm()
