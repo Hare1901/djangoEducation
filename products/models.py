5bc7bf1b0b0f7a7c5d971a878486ffd7f56fd269
hare1901 <hare1901@yandex.ru>
2023-05-08 08:13:32 +0300

    Создание модели Basket

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from users.models import User
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -15,7 +15,16 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=12, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Продукт: {self.name}| Категория: {self.category.name}"
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    products = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.email} | Продукт {self.products.name}'
