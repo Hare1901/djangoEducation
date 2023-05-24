277dc4c15030f0465828ac95ea894f73072bf301
hare1901 <hare1901@yandex.ru>
2023-05-24 06:42:17 +0300

    Products в класс

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from products.models import ProductCategory, Product, Basket
-from django.core.paginator import Paginator
-from  django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 # Create your views here.
 
@@ -12,27 +12,29 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data()                                                          #Выполняется код, возвращается словарь, который мы можем дополнить своим контекстом
-        context['title']: "Магазин"
+        context['title'] = 'Store - Каталог'
         return context
 
 
-def products(request, category_id=None, page_number=1):
-    if category_id:
-        products = Product.objects.filter(category_id=category_id)
-    else:
-        products = Product.objects.all(),
+class ProductsListView(ListView):
+    model = Product                                                                                                 #автоматически создает product_list, в котором все объекты Product
+    template_name = "products/products.html"
+    paginate_by = 3
+    def get_queryset(self):                                                                                         # переопределяем функцию, изначально возвращает Product.objects.all()
 
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
 
-    context = {
-        'title': "Store - Каталог",
-        'products': products_paginator,
-        'categories': ProductCategory.objects.all(),
-    }
-    return render(request, "products/products.html", context)
+        return queryset.filter(category_id=category_id) if category_id else queryset
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+
+        context = super(ProductsListView,self).get_context_data()
+        context['title'] = "Store - Каталог"
+        context['categories'] = ProductCategory.objects.all()
+
+        return context
+#
 
 @login_required
 def basket_add(request, product_id):
