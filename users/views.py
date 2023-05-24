907b8e551318b2632d96cb0794e052420c6194bc
hare1901 <hare1901@yandex.ru>
2023-05-24 14:36:57 +0300

    CreateViev и переопределяем контроллер регистрации и профиля

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
-from django.contrib import auth, messages
+from django.urls import reverse, reverse_lazy
+from django.contrib import auth
 from users.forms import UserLoginForm, UserRegistrationForm, UseProfileForm
 from products.models import Basket
-from django.contrib.auth.decorators import login_required
-
+from django.views.generic.edit import CreateView, UpdateView
+from users.models import User
 
 def login(request):
     if request.method == "POST":
@@ -22,37 +22,67 @@ def login(request):
     return render(request, "users/login.html", context)
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Аккаунт зарегистрирован!")
-            return HttpResponseRedirect(reverse("users:login"))
-    else:
-        form = UserRegistrationForm()
-    form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, "users/registration.html", context)
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UseProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            print(form.errors)
-    else:
-        form = UseProfileForm(instance=request.user)
+class UserRegistrationView(CreateView):
+    model = User
+    #указываем ссылку на созданый нами класс
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    #сохраняем куда перенаправит пользователя с помощью нового метода
+    success_url = reverse_lazy('users:login')
 
 
-    context = {'title': 'Store - Профиль',
-               'form': form,
-               'baskets': Basket.objects.filter(user=request.user)
-               }
-    return render(request, 'users/profile.html', context)
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data()
+        context['title'] = 'Регистрация'
+        return context
+
+
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Аккаунт зарегистрирован!")
+#             return HttpResponseRedirect(reverse("users:login"))
+#     else:
+#         form = UserRegistrationForm()
+#     form = UserRegistrationForm()
+#     context = {'form': form}
+#     return render(request, "users/registration.html", context)
+
+
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UseProfileForm
+    template_name = "users/profile.html"
+
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['title'] = 'Личный кабинет'
+        # корзину в контекст
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
+
+
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         form = UseProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('users:profile'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UseProfileForm(instance=request.user)
+#
+#
+#     context = {'title': 'Store - Профиль',
+#                'form': form,
+#                'baskets': Basket.objects.filter(user=request.user)
+#                }
+#     return render(request, 'users/profile.html', context)
 
 def logout(request):
     auth.logout(request)
