171e9a9e07c1a1f2d8e2aa500641b04b2d972961
hare1901 <hare1901@yandex.ru>
2023-05-30 20:25:12 +0300

    Создаем контекстный процессор( сделали baskets глобальной переменной, чтобы убрать def get_context)

--- users/views.py
+++ users/views.py
@@ -45,12 +45,6 @@ class UserProfileView(TitleMixin, UpdateView):
 
         return reverse_lazy('users:profile', args=(self.object.id,))
 
-    def get_context_data(self, **kwargs):
-
-        context = super(UserProfileView, self).get_context_data()
-        context['baskets'] = Basket.objects.filter(user=self.object)
-
-        return context
 
 
 class EmailVerificationsView(TitleMixin, TemplateView):
