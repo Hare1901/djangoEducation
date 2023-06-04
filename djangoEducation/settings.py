028da4065e834a536c7c79e515a79c4186c20b2e
hare1901 <hare1901@yandex.ru>
2023-06-04 08:16:40 +0300

    Подклбчение и настройка OAUTH под гитхаб

--- djangoEducation/settings.py
+++ djangoEducation/settings.py
@@ -36,6 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+
+
 
     "products",
     "users",
@@ -148,5 +156,25 @@ EMAIL_PORT = 465
 EMAIL_HOST_USER = "sadchicovIlia@yandex.ru"
 EMAIL_HOST_PASSWORD = '125689347Hare'
 EMAIL_USE_SSL = True
+
+
+# Настройки OAUTH
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        'SCOPE': [
+            'user',
+        ],
+    }
+}
+
 
 
